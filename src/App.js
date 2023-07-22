e89840688fa268315480a5da6cdb0f25ea719ce0
JPLACLAU <laclaujeanpaul@gmail.com>
2023-07-21 21:02:35 -0300

    mapa 2: v0.1

---
+++ src/App.js
@@ -0,0 +1,177 @@
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./assets/stylesheets/App.css";
+
+import Layout from "./components/Layout";
+
+import locations from "./data/locations";
+import utensilsIcon from "./assets/shared/utensils-marker.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
+const MAPBOX_USERID = process.env.REACT_APP_MAPBOX_USERID;
+const MAPBOX_STYLEID = process.env.REACT_APP_MAPBOX_STYLEID;
+
+function App() {
+  const mapRef = useRef();
+
+  useEffect(() => {
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+    });
+  }, []);
+
+  useEffect(() => {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+
+    if (!map) return;
+
+    map.eachLayer((layer = {}) => {
+      const { options } = layer;
+      const { name } = options;
+
+      if (name !== "Mapbox") {
+        map.removeLayer(layer);
+      }
+    });
+
+    const geoJson = new L.GeoJSON(locations, {
+      pointToLayer: (feature, latlng) => {
+        return L.marker(latlng, {
+          icon: new L.Icon({
+            iconUrl: utensilsIcon,
+            iconSize: [26, 26],
+            popupAnchor: [0, -15],
+            shadowUrl: markerShadow,
+            shadowAnchor: [13, 28],
+          }),
+        });
+      },
+      onEachFeature: (feature = {}, layer) => {
+        const { properties = {}, geometry = {} } = feature;
+        const { name, delivery, deliveryRadius, tags, phone, website } =
+          properties;
+        const { coordinates } = geometry;
+
+        let deliveryZoneCircle;
+
+        if (deliveryRadius) {
+          deliveryZoneCircle = L.circle(coordinates.reverse(), {
+            radius: deliveryRadius,
+          });
+        }
+
+        const popup = L.popup();
+
+        const html = `
+          <div class="restaurant-popup">
+            <h3>${name}</h3>
+            <ul>
+              <li>
+                ${tags.join(", ")}
+              </li>
+              <li>
+                <strong>¿Se encontraron microplásticos?:</strong> ${
+                  delivery ? "Sí" : "No"
+                }
+              </li>
+              <li>
+                <strong>Promedio de voluntarios:</strong> ${phone}
+              </li>
+              <li>
+                <strong>Organizador:</strong> <a href="${website}">${website}</a>
+              </li>
+            </ul>
+          </div>
+        `;
+
+        popup.setContent(html);
+
+        layer.bindPopup(popup);
+
+        layer.on("mouseover", () => {
+          if (deliveryZoneCircle) {
+            deliveryZoneCircle.addTo(map);
+          }
+        });
+
+        layer.on("mouseout", () => {
+          if (deliveryZoneCircle) {
+            deliveryZoneCircle.removeFrom(map);
+          }
+        });
+      },
+    });
+
+    geoJson.addTo(map);
+  }, [mapRef]);
+
+  useEffect(() => {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+
+    if (!map) return;
+
+    /**
+     * @lesson-10-todo Exercise 3
+     * If we want to fire custom functionality, how can we listen
+     * to events on the map for when a location is found?
+     */
+
+    /**
+     * @lesson-10-todo Extra Credit
+     * After setting our event handler, how can we make sure React
+     * cleans up those handlers when the component unmounts?
+     */
+  }, [mapRef]);
+
+  function handleOnSetLocation() {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+
+    const locationNationalGeographic = [38.90563, -77.037];
+
+    /**
+     * @lesson-10-todo Exercise 1
+     * We have our National Geographic Museum coordinates, but
+     * we want to be able to show someone where that is on the map.
+     * How can we create a marker and update our map to that location?
+     */
+  }
+
+  return (
+    <Layout>
+      <div className="search-actions">
+        <ul>
+          <li>
+            <button onClick={handleOnSetLocation}>
+              Muestreo de microplásticos en Argentina
+            </button>
+          </li>
+          {/**
+           * @lesson-10-todo Exercise 2
+           * Using a button for someone to set their location is
+           * a good way to let them understand where they are in
+           * relation to our restaurants. How can we find someone's
+           * location and add a marker when clicking on a button?
+           */}
+        </ul>
+      </div>
+      <Map ref={mapRef} center={[-45.0, -65.0]} zoom={5}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+        />
+      </Map>
+    </Layout>
+  );
+}
+
+export default App;
